d43b2b5e291faa76f0ee0bdcc2d3796ddc714c1d
milominderbinder22 <36114632+milominderbinder22@users.noreply.github.com>
2018-09-24 00:17:36 -0400

    Add files via upload

---
+++ jupyterlab.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\puter\Anaconda3\Scripts\activate C:\Users\puter\Anaconda3
+jupyter lab >C:\Users\puter\.anaconda\navigator\scripts\jupyterlab-out-2.txt 2>C:\Users\puter\.anaconda\navigator\scripts\jupyterlab-err-2.txt
